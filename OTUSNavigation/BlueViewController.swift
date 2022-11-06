66250338d4f1aaa8b9c6bd5bb3d4350bb76efb7a
Kirill Krainov-Timanovsky <79582042+kirillkrainov95@users.noreply.github.com>
2022-11-07 01:51:17 +0300

    colours vc

---
+++ OTUSNavigation/BlueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BlueViewController.swift
+//  OTUSNavigation
+//
+//  Created by Kirill on 07.11.2022.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ OTUSNavigation/ViewControllerTwo.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerTwo.swift
+//  OTUSNavigation
+//
+//  Created by Kirill on 07.11.2022.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
